4dae4e5f13f8c13686446e8561dfda968b39bb13
jake <jake@jober.io>
2021-07-15 16:11:02 +0900

    completion buedongsan

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,23 @@
   <Menu :menus="menus"/>
 
   <!-- 모달창 -->
-  <Modal 
-  @closeModal="modal_state = false"
-  :modal_number="modal_number" 
-  :modal_state="modal_state" 
-  :products="products" 
-  />
+  <transition name="fade">
+    <Modal 
+    @closeModal="modal_state = false"
+    :modal_number="modal_number" 
+    :modal_state="modal_state" 
+    :products="products" 
+    />
+  </transition>
 
   <!-- 할인 모달 -->
-  <Discount />
+  <Discount v-if="showDiscount == true" :sale="sale"/>
 
+
+  <button @click="Back">기본</button>
+  <button @click="priceLow">낮은 가격 순</button>
+  <button @click="priceHigh">높은 가격 순</button>
+  <button @click="ABC">이름 순</button>
   <!-- 상품 카드 -->
   <ItemList
   @openModal="modal_state = true, modal_number = i"
@@ -38,19 +45,50 @@ export default {
   name: 'App',
   data(){
         return {
+            showDiscount : true,
+            sale : 30,
+            originProducts : [...Item_info],
             modal_number : 0,
             modal_state : false,
             menus : ["Home", 'Products', "About"],
             products : Item_info
             }
     },
-  components: {
-    Discount : Discount,
-    ItemList : ItemList,
-    Modal : Modal,
-    Menu : Menu
+    methods: {
+      priceLow(){
+        this.products.sort(function(a,b){
+          return a.price - b.price
+        })
+      },
+      Back(){
+        this.products = [...this.originProducts];
+      },
+      priceHigh(){
+        this.products.sort(function(a,b){
+          return b.price - a.price
+        })
+      },
+      ABC(){
+        this.products.sort(function(a,b){
+          return a.title.localeCompare(b.title)
+        })
+      }
+    },
+    mounted(){
+      setInterval(()=>{
+        this.sale -= 1
+        if(this.sale == 0){
+          this.showDiscount = false
+        }
+      }, 1000)
+    },
+    components: {
+      Discount : Discount,
+      ItemList : ItemList,
+      Modal : Modal,
+      Menu : Menu
+    }
   }
-}
 </script>
 
 <style>
@@ -66,5 +104,23 @@ body {
 }
 div {
   box-sizing: border-box;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: all 1s;
+}
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,7 +4,8 @@
         <img :src="products[modal_number].image" >
         <h4>{{ products[modal_number].title }}</h4>
         <p>{{ products[modal_number].content }}</p>
-        <p>{{ products[modal_number].price }} 원</p>
+        <input  v-model="month">
+        <p> {{month}}개월 선택함 : {{ products[modal_number].price * month}} 원</p>
         <button @click="$emit('closeModal')">닫기</button>
         </div>
     </div>
@@ -13,10 +14,27 @@
 <script>
 export default {
     name: 'Modal',
+    data(){
+        return{
+            month : 1
+        }
+    },
     props : {
         modal_number : Object,
         modal_state : Object,
         products : Object
+    },
+    watch : {
+        month(a){
+            if(a >= 13){
+                alert('12이하의 숫자를 선택해주세요')
+                this.month=1
+            }
+            if(isNaN(a) == true){
+                alert('숫자를 입력해주세요')
+                this.month = 1
+            }
+        }
     }
 }
 </script>
@@ -26,6 +44,7 @@ export default {
   width: 100%; 
   height:100%;
   display: flex;
+  top: 0;
   align-items: center;
   justify-content: center;
   background: rgba(0,0,0,0.5);
